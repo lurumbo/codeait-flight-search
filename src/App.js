00ece2f4e1fcecf12e280862b1f7887b3bf53fa9
Lu Rumbo <lurumbo18@gmail.com>
2019-12-21 13:32:35 -0300

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Home Page</h1>
+        <article>
+          <section>
+            <h2>Promotions</h2>
+          </section>
+          <section>
+            <div>
+              <Link to="/search">Search</Link>
+            </div>
+          </section>
+        </article>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Flights from  './components/Flights';
+import Search from './components/Search';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/flights" component={Flights} />
+            <Route path="/search" component={Search} />
+            <Route component={NotFound} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
